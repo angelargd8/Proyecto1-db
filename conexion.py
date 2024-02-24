0087ebced37c2988d140e6abeb02130b551cabd2
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-02-24 14:17:08 -0600

    xd

--- conexion.py
+++ conexion.py
@@ -5,13 +5,15 @@ def conexiones():
     try:
         conexion= psycopg2.connect(
             host = "localhost",
-            database = "prueba1",
+            database = "Proyecto1",
             user = "postgres",
             password = "123456",
             port = "5432" #SELECT * FROM pg_settings WHERE name = 'port';
         )
         print("conexion exitosa")
+        return conexion
 
     except Exception as e:
         print(e)
         print("error en la conexion")
+        return None
--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,18 @@
 from conexion import conexiones
+import psycopg2
 
-conexiones()
+conexion= conexiones()
+#uso de cursor
+cursor= conexion.cursor()
+
+#queries sql
+sql='SELECT * FROM teams'
+#con lo que se ejecutan las consultas
+cursor.execute(sql)
+#mostrar el resultado
+registro= cursor.fetchall() 
+
+print(registro)
+#cerrando la conexión a la base de datos
+cursor.close()
+conexion.close()
